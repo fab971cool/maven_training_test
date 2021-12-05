10604fc3a5b0636da91fca2c5765e202b1b66866
Fabien Verspan <78422154+fab971cool@users.noreply.github.com>
2021-12-05 10:59:09 +0100

    Add test coverage

---
+++ src/main/java/fr/lernejo/Sample_test.java
@@ -0,0 +1,9 @@
+package fr.lernejo;
+
+public class Sample_test
+{
+    public static void main(String[] args)
+    {
+        System.out.println( "Ceci est une class de test");
+    }
+}
---
+++ src/test/java/fr/lernejo/Sample_testTest.java
@@ -0,0 +1,12 @@
+package fr.lernejo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Sample_testTest {
+
+    @Test
+    void main() {
+    }
+}
